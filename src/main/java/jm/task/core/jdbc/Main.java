0f64395da2a221f16c07f770321f2412ccb3f2ef
kimsofia95 <kimsofiamew@gmail.com>
2021-03-10 23:25:31 +0900

    create util method for connect with database

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,45 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+
+import java.sql.*;
+
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        User[] users = {new User("Masha", "Petrova", (byte) 20),  new User("Igor", "Alekseev", (byte) 25),
+                new User("Sasha", "Familly", (byte) 29), new User("Kolya", "Lil", (byte) 23)};
+        try {
+            String url = "jdbc:mysql://127.0.0.1:3306/study?useSSL=false&serverTimezone=Asia/Seoul";
+            String login = "root";
+            String password = "root";
+            Connection con = Util.DataBaseConnection("com.mysql.cj.jdbc.Driver", url, login, password);
+
+            String createTable = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), lastName VARCHAR(40), age INT(3))";
+            try {
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate(createTable);
+                for (int i = 0; i < users.length; i++) {
+                    stmt.executeUpdate("INSERT users(name, lastName, age) VALUES ('" + users[i].getName() + "', '" + users[i].getLastName() + "', " + users[i].getAge() + ")");
+                    System.out.println("User с именем - " + users[i].getName() + " добавлен в базу данных");
+                }
+
+                ResultSet rs = stmt.executeQuery("select * from users");
+                while (rs.next()) {
+                    String name = rs.getString("name") + " " + rs.getString("lastName");
+                    String age = rs.getString("age");
+                    System.out.println("Name: " + name + " Age: " + age);
+                }
+                stmt.executeUpdate("DELETE from users");
+                stmt.executeUpdate("DROP table IF EXISTS users");
+                rs.close();
+                stmt.close();
+            } finally {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection DataBaseConnection(String className, String url, String login, String password) {
+        try {
+            Class.forName(className).getDeclaredConstructor().newInstance();
+            Connection connection = DriverManager.getConnection(url, login, password);
+            return connection;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
